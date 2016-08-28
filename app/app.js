af886108a249b5c175a7b289f2beeff9aab80f0e
Brandon Sherette <brandonsherette@gmail.com>
2016-08-28 15:29:49 -0700

    initial commit

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import App from './components/app.component.jsx!';
+import ReactDom from 'react-dom';
+import React from 'react';
+
+ReactDom.render(
+  React.createElement(OuterComponent),
+  document.getElementById('app')
+);
---
+++ app/components/app.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderComponent from './header.component.jsx!';
+
+var AppComponent = React.createClass({
+  render: function() {
+    return (
+      <HeaderComponent />
+    );
+  }
+});
+
+export default AppComponent;
